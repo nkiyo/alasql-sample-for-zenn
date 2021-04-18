dd14fe5801fe795ddb1f0c82d8428030ad5e080d
nkiyo <nkiyo@hoge.com>
2021-04-19 08:50:15 +0900

    Update

--- sample.js
+++ sample.js
@@ -2,7 +2,7 @@ const alasql = require('alasql')
 
 const file = "タスクリスト.xls"
 const sheet = "タスク"
-const range = "B5:I100"
+const range = "B5:G100"
 alasql.promise(`SELECT * FROM XLS("${file}",{sheetid: "${sheet}", range: "${range}"}) WHERE [担当者] = "Bさん" AND [状況] = "完了"`)
   .then(res => {
     console.log(res);
